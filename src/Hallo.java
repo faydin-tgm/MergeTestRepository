d76fa7949e5c771cf07d4e92c964788476b33d72
Faydin <faydin@student.tgm.ac.at>
2017-03-14 11:03:38 +0100

    V1

---
+++ src/Hallo.java
@@ -0,0 +1,15 @@
+
+public class Hallo {
+
+	public static void gutenTag(){
+		System.out.println("Guten Tag!");
+	}
+	
+	public static void byby(){
+		System.out.println("Auf Wiedersehen.");
+	}
+	
+	public static void main(String[] args){
+		
+	}
+}
